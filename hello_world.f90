57a4c7bb727c5402bc0fa83e4233baddae46e23a
Mark Rhodes-smith <Mark-D-Smith@github.com>
2023-02-28 12:05:54 +0000

    Add date time

--- hello_world.f90
+++ hello_world.f90
@@ -1,4 +1,8 @@
 program hello_world
     implicit none
+    character(8) :: date
+    character(10) :: time
     print*, "Hello world!"
+    call date_and_time(date=date, time=time)
+    print*, date//" "//time
 end program hello_world
